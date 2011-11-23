7cd1aa5f5e49a1e5739194971a117e9718edce44
Igor S.K <garrikus@mail.ru>
2011-11-23 16:28:19 +0300

    N_Orion: Changes JFFS part's offset and size to match those of kernel.

--- include/configs/omap3_orion.h
+++ include/configs/omap3_orion.h
@@ -192,8 +192,8 @@
 /* nand device jffs2 lives on */
 #define CONFIG_JFFS2_DEV		"nand0"
 /* start of jffs2 partition */
-#define CONFIG_JFFS2_PART_OFFSET	0x680000
-#define CONFIG_JFFS2_PART_SIZE		0xf980000	/* sz of jffs2 part */
+#define CONFIG_JFFS2_PART_OFFSET	0x00780000
+#define CONFIG_JFFS2_PART_SIZE		0x3f880000	/* sz of jffs2 part */
 
 /* Environment information */
 #define CONFIG_BOOTDELAY	2
